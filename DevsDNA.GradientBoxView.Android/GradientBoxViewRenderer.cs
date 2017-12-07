ba121fcbcc6c110639908496caf72647b3c21acc
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2017-12-07 17:41:08 +0100

    Decreased Xamarin.Forms version as dependency

--- DevsDNA.GradientBoxView.Android/GradientBoxViewRenderer.cs
+++ DevsDNA.GradientBoxView.Android/GradientBoxViewRenderer.cs
@@ -5,15 +5,11 @@ using Xamarin.Forms;
 [assembly: ExportRenderer(typeof(GradientBoxView), typeof(GradientBoxViewRenderer))]
 namespace DevsDNA.GradientBoxViewAndroid
 {
-    using Android.Content;
     using Android.Graphics;
     using Xamarin.Forms.Platform.Android;
 
     public class GradientBoxViewRenderer : VisualElementRenderer<GradientBoxView>
     {
-        public GradientBoxViewRenderer(Context context) : base(context)
-        { }
-
         protected override void DispatchDraw(Canvas canvas)
         {
             var gradient = new LinearGradient(0, 0, 0, Height, 
