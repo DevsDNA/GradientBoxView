f8ab7be09e04759b37b618afec6ca7fbe0b56a34
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2017-12-13 17:08:18 +0100

    Updated example

--- Examples/GradientBoxViewExample/GradientBoxViewExamplePage.xaml.cs
+++ Examples/GradientBoxViewExample/GradientBoxViewExamplePage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using DevsDNA;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace GradientBoxViewExample
 {
@@ -9,35 +7,6 @@ namespace GradientBoxViewExample
         public GradientBoxViewExamplePage()
         {
             InitializeComponent();
-
-            CreateGradientMatrix();
-        }
-
-        void CreateGradientMatrix()
-        {
-            var rows = 16;
-            var columns = 16;
-
-            for (int i = 0; i < rows; i++)
-            {
-                var rowDefinition = new RowDefinition();
-                grid.RowDefinitions.Add(rowDefinition);
-
-                var columnDefinition = new ColumnDefinition { Width = GridLength.Star };
-                grid.ColumnDefinitions.Add(columnDefinition);
-
-                for (int j = 0; j < columns; j++)
-                {
-                    var hue = (1d / columns) * j;
-                    var saturation = (1d / rows) * i;
-                    var gradientBoxView = new GradientBoxView
-                    {
-                        TopColor = Color.White,
-                        BottomColor = Color.FromHsla(hue, 0.5d, 0.5d)
-                    };
-                    grid.Children.Add(gradientBoxView, i, j);
-                }
-            }
         }
     }
 }
